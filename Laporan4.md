9bdf1d32f5440ddc8ba23653bf8424c2bcc47574
hilyatuljannah <hilyatuljannah926@gmail.com>
2024-03-10 16:19:35 +0700

    praktikum 2.2

---
+++ Laporan4.md
@@ -0,0 +1,5 @@
+# $fillable
+
+1. ![alt text](<Screenshot 2024-03-08 084429.png>)
+2. ![alt text](<Screenshot 2024-03-08 084708.png>)
+3. ![alt text](<Screenshot 2024-03-08 084820.png>)
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,21 +4,35 @@ namespace App\Http\Controllers;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_dua',
-            'nama' => 'Manager 2',
-            'password' => Hash::make('12345'),
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345'),
             
-        ];
-        UserModel::create($data);
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::find(1);
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::where('level_id', 1)->first();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::findOr(20, ['username', 'nama'], function() {
+        //     abort(404);
+        // });
+        // $user = UserModel::findOrFail(1);
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
     }
 }
