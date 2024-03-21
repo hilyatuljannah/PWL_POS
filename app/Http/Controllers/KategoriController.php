8f9994c255b886878309c91bc7bb1980e4a86373
hilyatuljannah <hilyatuljannah926@gmail.com>
2024-03-21 14:19:42 +0700

    tugas 1

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,27 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\DataTables\KategoriDataTable;
+use App\Models\KategoriModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-    public function index()
+    public function index(KategoriDataTable $dataTable)
     {
-      /*  $data = [
-            'kategori_kode' => 'K6',
-            'kategori_nama' => 'Canvas',
-            'created_at' => now()
-        ];
-        DB::table('m_kategori')->insert($data);
-        return 'Insert data baru berhasil'; 
-        $row = DB::table('m_kategori')->where('kategori_kode', 'K6')->update(['kategori_nama' => 'kanvas']);
-        return 'Update data berhasil. Jumlah data yang diupdate : ' . $row. ' baris'; 
-
-        $row = DB::table('m_kategori')->where('kategori_kode', 'K6')->delete();
-        return 'Delete data berhasil. Jumlah data yang dihapus : ' . $row.' baris'; */
-
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data'=>$data]);
+        return $dataTable->render('kategori.index');
     }
+
+    public function create()
+    {
+        return view ('kategori.create');
+    }
+
+    public function store (Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+        return redirect('/kategori');
+    }
+
+
+
+    // public function index()
+    // {
+    //   /*  $data = [
+    //         'kategori_kode' => 'K6',
+    //         'kategori_nama' => 'Canvas',
+    //         'created_at' => now()
+    //     ];
+    //     DB::table('m_kategori')->insert($data);
+    //     return 'Insert data baru berhasil'; 
+    //     $row = DB::table('m_kategori')->where('kategori_kode', 'K6')->update(['kategori_nama' => 'kanvas']);
+    //     return 'Update data berhasil. Jumlah data yang diupdate : ' . $row. ' baris'; 
+
+    //     $row = DB::table('m_kategori')->where('kategori_kode', 'K6')->delete();
+    //     return 'Delete data berhasil. Jumlah data yang dihapus : ' . $row.' baris'; */
+
+    //     // $data = DB::table('m_kategori')->get();
+    //     // return view('kategori', ['data'=>$data]);
+    // }
 }
