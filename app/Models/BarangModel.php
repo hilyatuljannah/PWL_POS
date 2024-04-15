820f30d110363177f5c8fc7d8233e5ffbd94f15e
hilyatuljannah <hilyatuljannah926@gmail.com>
2024-04-15 10:47:12 +0700

    job 7 fixed

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -6,16 +6,9 @@
 namespace App\Models;
 
 use App\Models\KategoriModel;
-
-
-
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
 use Illuminate\Database\Eloquent\Model;
-
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 
